43116218bb1fadd7de4196f2c11db47405cdffae
Andrew Cho and Taylor Grohs <cho-andrew@hotmail.com>
2016-04-19 11:28:32 -0700

    add integrated security for sql server

---
+++ src/sports-league/Models/SportsLeagueContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sports_league.Models
+{
+    public class SportsLeagueContext : DbContext
+    {
+        public virtual DbSet<Team> Teams { get; set; }
+        protected override void OnConfiguring (DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SportsLeague;integrated security = True");
+        }
+    }
+}
